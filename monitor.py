d3e798656f492c1c642c3c070f3d463a6195eaf0
bsizwe <bsizwe@gmail.com>
2026-05-22 11:31:20 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -597,5 +597,8 @@ def logout():
     session.clear()
     return redirect("/login")
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5050, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
